b5acbe76d84fac1517cf8f924e49bf2a92387704
Moopen <n.moopen@uu.nl>
2023-11-21 08:16:12 +0100

    fixed today's chapters

---
+++ exercises/code-readability/pep8.py
@@ -0,0 +1,7 @@
+### This is an important function
+some_constant=1
+def MYFUNCTION (X ,y = 1): 
+ z="1",;y=y+1
+ return (
+        X+y  <  some_constant) == True  #This is a very long comment that about how the condition can be checked directly in the return statement.
+from numpy import *
